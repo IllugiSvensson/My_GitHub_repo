391ea3dbb43fa4267c8f1d745f4e81e6977e0f38
IllugiSvensson <woljin1@mail.ru>
2019-07-16 10:58:46 +0300

    Поменять кодировку

---
+++ Проекты на С++/categorizer/build/CMakeFiles/categorizer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/categorizer.dir/src/categorizer.cpp.o"
+  "categorizer.pdb"
+  "categorizer"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/categorizer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
