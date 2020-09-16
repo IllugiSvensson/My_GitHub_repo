fff0200d05eb6ad712ea92c1b53b62ee5427d88e
illugisvensson <illugisvensson@gmail.com>
2020-09-16 16:29:17 +0300

    Добавил скрипт настройки микрофона

---
+++ Скрипты на Bash/micsetup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+amixer -D default -- sset PCM -5dB
+amixer -D default -- sset Surround -4dB
+amixer -D default -- sset Center -4dB
+amixer -D default -- sset LFE -4dB
+amixer -D default -- sset Side -4dB
+amixer -D default -- sset Line 8dB
+amixer -D default sset 'Line Boost' 20dB
+amixer -D default sset Capture 12dB
+amixer -D default sset Digital 10dB
+amixer -D default sset 'Input Source' Line
+
+alsactl store
