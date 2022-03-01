efff4c50d7e6c5da003542b44815a76e22e2d270
Смирнов Александр <sad@nita.ru>
2022-03-01 11:47:43 +0300

    Проект Backupper. Добавил диалог для очистки копий и логирование бекапов

--- Скрипты на Bash/Backupper.sh
+++ Скрипты на Bash/Backupper.sh
@@ -19,80 +19,95 @@ function BackUp {
 
 	fi
 	#Создаем архив с текущей датой
-	tar -cf /data/tmp/backups/${Prefix}_D${Date}_T$Time.tar -g /data/tmp/backups/$d $1
+	tar -cf /data/tmp/backups/$1/${Prefix}_D${Date}_T$Time.tar -g /data/tmp/backups/$1/$d $1
 	echo "Архив ${Prefix}_D${Date}_T$Time.tar создан"
 
 }
 
 
+
+
+
 #Проверка аргументов
 frs=`echo $1 | egrep -o "[0-9]{1,}[smh]{1}"`
-scd=`echo $2 | egrep -o "(\/{1}|((\/{1}\w+\/?)+([\.|\-]{1}\w+)?))"`
-
+scd=`echo $2 | egrep -o "(\/{1}|((\/{1}\w+\/?)+([\.|\-]{1}\w+)?))" | sed 's/[\/]$//'`
 #Справка по запуску
-if [ "$1" == "--help" ]
+if [ "$#" -ne 2 ] || [ "$1" != "$frs" ] || [ ! -e "$scd" ]
 then
 
 	echo " "
-	echo "Скрипт для периодического создания бекапов"
-	echo "при возникновении изменений в каталогах"
-	echo "Формат запуска: ./Baсkupper время каталог"
+	echo " Скрипт для периодического создания бекапов"
+	echo " при возникновении изменений в каталогах"
+	echo " Формат запуска: ./Baсkupper время каталог"
 	echo "	время: период проверки наличия изменений"
 	echo "	    формат: ЧИСЛО[минуты] Xm или ЧИСЛО[часы] Xh"
 	echo "	каталог: путь до архивируемого каталога"
 	echo "	    формат: /каталог/файл или /файл или /файл.тип"
-	echo "Пример: ./Backupper.sh 60m /soft"
+	echo " Пример: ./Backupper.sh 60m /soft"
 	echo " "
-	exit
-
-elif [ "$#" -ne 2 ] || [ "$1" != "$frs" ] || [ "$2" != "$scd" ]
-then
-
-	#Подсказка в случае ошибки
-	echo " "
-	echo "Справка по запуску. --help"
-	echo " "
-	exit
+	exit 0
 
 fi
 
 
+
 #Запускаем скрипт в фоновом режиме
+Flag=0
 while true
 do
 
 	#Создание первого архива, от которого пойдут проверки
 	#Создаем каталог бекапов если такого нет
-	mkdir /data/tmp/backups
+	[ -e "/data/tmp/backups/$scd" ] || { mkdir -p /data/tmp/backups/$scd; }
+
+	#Префикс имени это название каталога, который резервируется
 	Prefix=`echo $2 | sed 's/\W/-/g' | cut -c2-`
-	if [ -z `ls /data/tmp/backups | egrep ${Prefix}_D.*.tar` ]
+	if [ -z `ls /data/tmp/backups/$scd | egrep ${Prefix}_D.*.snar` ]
 	then
 		#Если архива не существует, создаем новый(первый)
-		touch $2/changes.txt
-		ls -R -l -t -I changes.txt $2 | egrep -v inventory.txt > $2/inventory.txt
-		BackUp $2 now
+		touch $scd/changes.txt	#Проводим инвентаризацию ПО
+		ls -R -l -t -I changes.txt $2 | egrep -v inventory.txt > $scd/inventory.txt
+		BackUp $scd now
+
+	else
+
+		#Определяем самый последний существующий бекап
+		lastBackup=`ls -t /data/tmp/backups/$scd | grep ${Prefix}_D.*.tar | head -1`
+		lastSnar=`ls -t /data/tmp/backups/$scd | grep ${Prefix}_D.*.snar | head -1`
+		#Вынимаем список каталогов из архива и создаем новый
+		str=`echo $2 | cut -c2-`
+		tar -xf /data/tmp/backups/$scd/$lastBackup -C /data/tmp/backups $str/inventory.txt
+		tar -xf /data/tmp/backups/$scd/$lastBackup -C /data/tmp/backups $str/changes.txt
+		ls -R -l -t -I changes.txt $2 | egrep -v inventory.txt > $scd/inventory.txt
+		#Сравниваем оба списка
+		echo " " >> $2/changes.txt
+		echo " " >> $2/changes.txt
+		date >> $2/changes.txt
+		diff -r -u1 /data/tmp/backups/$str/inventory.txt $2/inventory.txt >> $2/changes.txt
+		if [ `echo $?` != 0 ]
+		then
+
+			#Если есть изменения, создаем новый архив
+			cat /data/tmp/backups/$str/changes.txt >> $2/changes.txt
+			BackUp $2 $lastSnar
+
+		fi
 
 	fi
 
-	#Определяем самый последний существующий бекап
-	lastBackup=`ls -t /data/tmp/backups | grep ${Prefix}_D.*.tar | head -1`
-	lastSnar=`ls -t /data/tmp/backups | grep ${Prefix}_D.*.snar | head -1`
-	#Вынимаем список каталогов из архива и создаем новый
-	str=`echo $2 | cut -c2-`
-	tar -xf /data/tmp/backups/$lastBackup -C /data/tmp/backups $str/inventory.txt
-	ls -R -l -t -I changes.txt $2 | egrep -v inventory.txt > $2/inventory.txt
-	touch $2/changes.txt
-	#Сравниваем оба списка
-	diff -ru /data/tmp/backups/$str/inventory.txt $2/inventory.txt > $2/changes.txt
-	if [ `echo $?` != 0 ]
+	#Спрашиваем, чистить ли последовательность бекапов
+	if [ `date +%a` == "Вс" -a $Flag == "0" ]
 	then
 
-		#Если есть изменения, создаем новый архив
-		BackUp $2 $lastSnar
+		killall -9 zenity
+		zenity --question --text="Удалить созданные бекапы?" --ellipsize
+		[ $? == "0" ] && { rm -rf /data/tmp/backups/$str; }
+		Flag=1
 
 	fi
-	rm $2/inventory.txt $2/changes.txt
 
-	sleep $1
+	[ `date +%a` == "Пн" ] && { Flag=0; }
+	rm $2/inventory.txt $2/changes.txt
+	sleep $frs
 
 done
