46243bc34d49e1db6049bea1a489545c314e3e5f
Смирнов Александр <sad@nita.ru>
2022-12-21 11:35:50 +0300

    Синхронизация

--- Скрипты на Bash/COM_init.sh
+++ Скрипты на Bash/COM_init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#Инициализация портов на Астра 1.6
-for i in {0..7}; do mknod /dev/ttyD$i c 19 $i; done
+#Инициализация портов menlinux compci
+for i in {0..7}; do mknod /dev/ttyD$i c 19 $i; done #8 портов
 modprobe men_bb_d203_a24
 modprobe men_mdis_kernel
-mdis_createdev -b d203_a24_1
+mdis_createdev -b d203_a24_1								
 modprobe men_lx_m77 devName=m77_1 brdName=d203_a24_1 slotNo=0 mode=2,4,2,2 echo=0,0,0,0
-
-
+						#mode 2 - RS422, 4 - RS485, 7 - RS232
+						#Задаем для первых 4х портов
