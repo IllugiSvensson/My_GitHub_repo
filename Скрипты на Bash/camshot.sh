ccc19d6c0ed40f5114b849653d71906e10d3574a
Смирнов Александр <sad@nita.ru>
2024-11-06 11:21:54 +0300

    Скрипты для нарезки фоток

---
+++ Скрипты на Bash/camshot.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export DISPLAY=:0
+path=/mnt/archive/CCTVfrags
+out=/mnt/archive/Camshot
+per=5000
+algo=mog
+res=mask
+
+channels="H-ERS1-TV-1 H-ERS1-IR-1"
+launch camshot --path $path/$1 --out $out/$1 --use_md --md_period $per --md_algoritms $algo --md_result $res --use_md_filters --channels=$channels --time 30
+    echo "$channels end!"
+
+channels="H-ERS1-TV-2 H-ERS1-TV-9 H-ERS1-IR-7"
+launch camshot --path $path/$1 --out $out/$1 --use_md --md_period $per --md_algoritms $algo --md_result $res --use_md_filters --channels=$channels --time 15
+    echo "$channels end!"
+
+channels="H-ERS1-TV-6 H-ERS1-TV-7 H-ERS1-IR-4 H-ERS1-IR-5 H-ERS1-IR-6"
+launch camshot --path $path/$1 --out $out/$1 --use_md --md_period $per --md_algoritms $algo --md_result $res --use_md_filters --channels=$channels --time 5
+    echo "$channels end!"
+
+channels="H-ERS1-TV-3 H-ERS1-TV-4 H-ERS1-TV-5 H-ERS1-TV-8 H-ERS1-TV-10 H-ERS1-IR-2 H-ERS1-IR-3 H-ERS1-IR-8"
+launch camshot --path $path/$1 --out $out/$1 --use_md --md_period $per --md_algoritms $algo --md_result $res --use_md_filters --channels=$channels --time 10
+    echo "$channels end!"
---
+++ Скрипты на Bash/masher.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+for n in /mnt/archive/Camshot/$1
+do
+    cd $n/Frames
+    for i in `ls | grep "video"`
+    do
+	cp -r $i/* ${i::-6}
+	rm -rf $i
+    done
+
+    cd $n/Masks
+    for i in `ls | grep "video"`
+    do
+	cp -r $i/* ${i::-6}
+	rm -rf $i
+    done
+
+    rm -rf $n/Rects
+
+done
