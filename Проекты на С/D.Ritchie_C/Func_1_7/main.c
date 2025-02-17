50fe406286e9f8e4cdfebfe8ba5ff9d3ead73b39
illugisvensson <woljin1@mail.ru>
2025-02-17 22:57:50 +0300

    Уроки по Си

---
+++ Проекты на С/D.Ritchie_C/Func_1_7/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+//Прототип функции
+int power(int m, int n);
+
+int main()
+{
+
+    for (int i = 0; i < 10; ++i) //Вызов функции два раза по значениям
+        printf("%d %d %d\n", i, power( 2, i), power(-3, i));
+    return 0;
+}
+
+//Описание функции
+//Возращаемый тип, имя, список параметров и их типы
+int power(int base, int n)
+{
+    int p = 1;
+    for (int i = 1; i <= n; ++i)
+        p = p * base;
+    //Возвращаемое значение
+    return p;
+}
